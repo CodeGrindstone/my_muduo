13b624e43a9cf07191e5e99a0ece8ed0f8dd1ea8
CodeGrindstone <3143598245@qq.com>
2024-11-25 11:52:07 +0000

    总结noncopyable的作用

--- noncopyable.h
+++ noncopyable.h
@@ -2,9 +2,9 @@
 
 
 /*
-* noncopyable不允许在类外部构造和析构
-* noncopyable被继承以后，派生类可以类内部可以正常调用noncopyable的构造和析构
-* 但是无法在外部调用拷贝构造和拷贝赋值
+noncopyable作用：
+    1. noncopyable不允许在类外部构造和析构
+    2. 继承noncopyable的类，不允许拷贝和赋值操作
 */
 class noncopyable
 {
